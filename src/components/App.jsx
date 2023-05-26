729731c835fa60008d45a01cb3a82985fa6a47bf
TIAGO MATIAS DE ARAUJO <tiagoaraujosud@gmail.com>
2023-05-26 10:04:50 -0300

    Add Notes to Note Area

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,31 @@ import Note from "./Note";
 import Footer from "./Footer";
 import CreateNoteArea from "./CreateNoteArea";
 import notes from "../notes";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateNoteArea />
-      {notes.map(note => (
-        <Note 
-          key={note.key} 
-          title={note.title} 
-          content={note.content}
+      <CreateNoteArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            id={index}
+            key={index} 
+            title={note.title} 
+            content={note.content}
         />
-      ))}
+        );
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateNoteArea.jsx
+++ src/components/CreateNoteArea.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateNoteArea() {
+function CreateNoteArea(props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+          title: "",
+          content: ""
+        });
+        event.preventDefault();
+    }
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+    
+        setNote(prevNote => {
+          return {
+            ...prevNote,
+            [name]: value
+          };
+        });
+    }
+
     return (
         <div>
             <form>
-                <input name="title" placeholder="Title"/>
-                <input name="content" type="text" placeholder="Take a note..."/>
-                <button>Add</button>
+                <input onChange={handleChange} name="title" placeholder="Title"/>
+                <textarea onChange={handleChange} name="content" type="text" placeholder="Take a note..."/>
+                <button onClick={submitNote}>Add</button>
             </form>
         </div>
     )
